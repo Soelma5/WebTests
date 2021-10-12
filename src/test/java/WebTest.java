5832f09a2c94238efd4fc2b66169576ecf69e9f6
Soelma5 <sanzhieva2020@gmail.com>
2021-10-12 00:08:39 -0400

    WebTests project's first commit

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebTest {
+        @Test
+        public void testFirst() throws InterruptedException {
+            String chromeDriver = "webdriver.chrome.driver";
+            String driverPath = "/Users/bulatsanzhiev/Downloads/chromedriver";
+            String url = "http://www.99-bottles-of-beer.net/";
+            String expectedResult = "http://www.99-bottles-of-beer.net/";
+
+
+            System.setProperty(chromeDriver, driverPath);
+
+            WebDriver driver = new ChromeDriver();
+
+            driver.get(url);
+            Thread.sleep(3000);
+
+            String actualResult = driver.getCurrentUrl();
+
+            driver.close();
+            driver.quit();
+
+            Assert.assertEquals(actualResult, expectedResult);
+        }
+    }
+
